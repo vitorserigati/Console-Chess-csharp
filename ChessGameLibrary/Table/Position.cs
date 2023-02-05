0f83ffefdb06a17d793a15c51ff1880ddcbad681
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2023-02-05 16:09:48 -0300

    Created Position Class

---
+++ ChessGameLibrary/Table/Position.cs
@@ -0,0 +1,18 @@
+namespace ConsoleChessLibrary.Table;
+
+class Position
+{
+    public int Line {get; set;}
+    public int Column {get; set;}
+    
+    public Position(int line, int column)
+    {
+        Line = line;
+        Column = column;
+    }
+
+    public override string ToString()
+    {
+        return $"{Line}, {Column}";
+    }
+}
